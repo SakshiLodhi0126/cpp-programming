19a748e90da54e237d5bc7ac16a18d96e71d9da2
Sakshi Lodhi <lodhisakshi645@gmail.com>
2026-01-27 11:46:22 +0530

    Notes

--- pattern_Printing/right_Angled_Triangle.cpp
+++ pattern_Printing/right_Angled_Triangle.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main() {
   int n;
-  cout << "Enter the number of rows and columns: ";
+  cout << "Enter the number of rows: ";
   cin >> n;
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
+    for (int j = 0; j < i + 1; j++) {
       cout << "*";
     }
     cout << endl;
