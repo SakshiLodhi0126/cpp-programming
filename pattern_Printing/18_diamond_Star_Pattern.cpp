77ed8be4955eb80296ecba38b0e881e8b7e70b87
Sakshi Lodhi <lodhisakshi645@gmail.com>
2026-02-13 20:02:07 +0530

    Notes

--- pattern_Printing/18_diamond_Star_Pattern.cpp
+++ pattern_Printing/18_diamond_Star_Pattern.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
-    cout << "Enter the number of rows: ";
-    cin >> n;
-    // upper half of diamond
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n - i - 1; j++)
-            cout << " ";  
-        for (int j = 0; j < 2*i + 1; j++)
-            cout << "*";  
-        cout << endl;
-    }
-    // lower half of diamond
-    for (int i = n-2; i >= 0; i--) {
-        for (int j = 0; j < n - i - 1; j++)
-            cout << " ";  
-        for (int j = 0; j < 2*i + 1; j++)
-            cout << "*";  
-        cout << endl;
-    }
-    return 0;
+  int n;
+  cout << "Enter the number of rows: ";
+  cin >> n;
+  // upper half of diamond
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n - i - 1; j++)
+      cout << " ";
+    for (int j = 0; j < 2 * i + 1; j++)
+      cout << "*";
+    cout << endl;
+  }
+  // lower half of diamond
+  for (int i = n - 2; i >= 0; i--) {
+    for (int j = 0; j < n - i - 1; j++)
+      cout << " ";
+    for (int j = 0; j < 2 * i + 1; j++)
+      cout << "*";
+    cout << endl;
+  }
+  return 0;
 }
---
+++ pattern_Printing/46_pattern_With_Zeroes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      if (j == 1 || i == j) {
+        cout << i << " ";
+      } else {
+        cout << 0 << " ";
+      }
+    }
+    cout << endl;
+  }
+  return 0;
+}
