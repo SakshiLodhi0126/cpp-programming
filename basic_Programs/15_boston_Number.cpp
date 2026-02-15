431f106ddeb8315ae6083b8c59ebf18755a3bffe
Sakshi Lodhi <lodhisakshi645@gmail.com>
2026-02-15 19:35:03 +0530

    Notes

--- basic_Programs/15_boston_Number.cpp
+++ basic_Programs/15_boston_Number.cpp
@@ -1,39 +1,39 @@
 #include <iostream>
 using namespace std;
-  int digitSum(int n) {
-    int sum = 0;
-    while (n != 0) {
-      int k = n % 10;
-      sum + = k;
-      n /= 10;
-    }
-    return sum;
+int digitSum(int n) {
+  int sum = 0;
+  while (n != 0) {
+    sum += n % 10;
+    n /= 10;
   }
-  bool prime(int n) {
-    int sum = 0;
-    for (int i = 2; i * i <= n; i++) {
-      if (n % i == 0) {
-        return false;
+  return sum;
+}
+bool prime(int n) {
+  if (n <= 1)
+    return false; // added this to handle 1 and 0
+  for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0)
+      return false;
+  }
+  return true;
+}
+int main() {
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  int original = n; // preserve original number
+  int sum = digitSum(n);
+  int factor = 0;
+  for (int i = 2; i <= original; i++) {
+    if (prime(i)) {
+      int temp = n; // use temp to avoid modifying n
+      while (temp % i == 0) {
+        factor += digitSum(i);
+        temp /= i;
       }
     }
-    return true;
   }
-  int main() {
-    int n;
-    cout << "Enter a number: ";
-    cin >> n;
-    int sum = 0;
-    int factor = 0;
-    sum = digitSum(n);
-    for (int i = 2; i <= n; i++) {
-      while (n != 0) {
-        if (prime(i)) {
-          factor += digitSum(i);
-        }
-        n /= i;
-      }
-    }
-    cout << sum << endl;
-    cout << factor << endl;
-  }
+  cout << sum << endl;
+  cout << factor << endl;
   return 0;
+}
