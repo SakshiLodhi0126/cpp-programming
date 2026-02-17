222a0e855333992541962647ed56ae2c30396f6b
Sakshi Lodhi <lodhisakshi645@gmail.com>
2026-02-17 14:22:12 +0530

    Notes

--- basic_Programs_2/20_chewbacca_Number.cpp
+++ basic_Programs_2/20_chewbacca_Number.cpp
@@ -1,35 +1,32 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
-    cout<<"Enter a number: ";
-    cin>>n;
-    int original = n;
-    int inverse=0;
-    int reverse = 0;
-    while(n!=0){
-        int digit = n%10;
-        reverse = reverse*10+digit;
-        n/=10;
+  int n;
+  cout << "Enter a number: ";
+  cin >> n;
+  int original = n;
+  int inverse = 0;
+  int reverse = 0;
+  while (n != 0) {
+    int digit = n % 10;
+    reverse = reverse * 10 + digit;
+    n /= 10;
+  }
+  while (reverse != 0) {
+    int digit = reverse % 10;
+    if (digit < 9 - digit) {
+      inverse = inverse * 10 + digit;
+    } else if (digit == 9) {
+      inverse = inverse * 10 + 9;
+    } else {
+      inverse = inverse * 10 + 9 - digit;
     }
-    while(reverse != 0){
-        int digit = reverse%10;
-        if(digit < 9 - digit){
-            inverse = inverse*10+digit;
-        }
-        else if(original < 10 and digit == 9){
-            inverse = inverse*10+9;
-        }
-        else{
-            inverse = inverse*10+9-digit;
-        }
-        reverse/=10;
-    }
-    if(inverse < original){
-        cout<<inverse;
-    }
-    else{
-        cout<<original;
-    }
-    return 0;
+    reverse /= 10;
+  }
+  if (inverse < original) {
+    cout << inverse;
+  } else {
+    cout << original;
+  }
+  return 0;
 }
