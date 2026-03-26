10aca740e1702c594e407e24bf2ca210a8c01c69
Sakshi Lodhi <lodhisakshi645@gmail.com>
2026-03-26 08:13:46 +0530

    Notes

---
+++ 11_Leetcode/2520_count_the_digits_that_divide_a_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num;
+  cin >> num;
+
+  int original = num;
+  int count = 0;
+
+  while (num > 0) {
+    int digit = num % 10;
+
+    if (original % digit == 0) {
+      count++;
+    }
+
+    num = num / 10;
+  }
+
+  cout << count;
+
+  return 0;
+}
